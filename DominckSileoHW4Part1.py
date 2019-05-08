d1140678df1dd119ba9f35b79eababb51e55e05c
dsileo <41395805+dsileo@users.noreply.github.com>
2019-05-07 20:32:54 -0700

    Part 1 of HW 4
    Thank you for the extension Stephan. I really appreciate it.

---
+++ DominckSileoHW4Part1.py
@@ -0,0 +1,3 @@
+#Homework 4 Part 1
+myVegetables = ['carrot', 'lettuce', 'onion', 'radish']
+print(myVegetables[0] + ", " +  myVegetables[1] + ", " +   myVegetables[2] + ", " + "and " +  myVegetables[3])
